015edcea8d05d27f3aca584f045e8932b9e1ee93
alexislendechy <135558007+alexislendechy@users.noreply.github.com>
2023-11-14 12:56:31 -0600

    image changed + '´s removed

--- src/Sections/AboutMe.jsx
+++ src/Sections/AboutMe.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import profile from "../assets/profile.jpg"
+import profile from "../assets/profile.png"
 
 export function AboutMe() {
     return (
@@ -8,16 +8,16 @@ export function AboutMe() {
             <img src={profile} alt="ALL" style={{ width: "250px", height: "250px", borderRadius: "30%"}} />
             <br /> <br />
             <p>
-                Hi, I'm Alexis Lendechy, a Chemical Engineer who has decided to move to the coding world and face 
-                new challenges here, which from what I've seen so far, there are plenty; it never ceases.
-                <br /> <br /> I've goot some experience working as Chemical Engineer in water treatment, inverse osmosis, 
+                Hi, I am Alexis Lendechy, a Chemical Engineer who has decided to move to the coding world and face 
+                new challenges here, which from what I have seen so far, there are plenty; it never ceases.
+                <br /> <br /> I have got some experience working as Chemical Engineer in water treatment, inverse osmosis, 
                 hazardous waste management among others. <br /> <br /> As CSR I worked in several campaings such as:
                 Billing, Account management, Downgrade. - Promo down, Change of services, Retention. <br /> <br />
-                When I moved to the IT world I've stared as Dispute Resolver: Direct and indirect accounts management,
+                When I moved to the IT world I have stared as Dispute Resolver: Direct and indirect accounts management,
                 Guidelines for returns and physical claims, Analysis of information such as: taxes, shipping & handling, 
                 pricing, contracts, legal name, web-invoicing, proceed with transactions on SAP and S4, among others.
-                <br /> <br /> Now in the coding world I'm about the be certificated as Full Stack Web Developer and it's been
-                a real journey. What a challenge to move to a completely different area, but I've enjoyed it. 
+                <br /> <br /> Now in the coding world I am about the be certificated as Full Stack Web Developer and it has been
+                a real journey. What a challenge to move to a completely different area, but I have enjoyed it. 
             </p>
         </section>
     );
