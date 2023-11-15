5c3b14efa6f5af3e22acfbf2acbc7eac9bf5dd5c
alexislendechy <135558007+alexislendechy@users.noreply.github.com>
2023-11-14 19:55:30 -0600

    images added + more links

--- src/Sections/Portfolio.jsx
+++ src/Sections/Portfolio.jsx
@@ -1,4 +1,13 @@
 import React from "react";
+import note from "../assets/note.png"
+import employee from "../assets/employee.jpeg"
+import social from "../assets/social.png"
+import text from "../assets/text.png"
+import logo from "../assets/logo.png"
+import read from "../assets/read.png"
+
+
+
 
 export function Portfolio () {
     return(
@@ -6,16 +15,34 @@ export function Portfolio () {
             <h2>Portfolio</h2>
             <div className="grid-container">
             <div className="grid-item">
-            <a href="https://github.com/alexislendechy/NoteTaker"></a>
+            <a href="https://github.com/alexislendechy/NoteTaker">
+                <img className="portfolio-image" src={note} alt="Note Taker" />
+            </a>
             </div>
             <div className="grid-item">
-            <a href="https://github.com/alexislendechy/EmployeeTracker"></a>
+            <a href="https://github.com/alexislendechy/EmployeeTracker">
+            <img className="portfolio-image" src={employee} alt="Employee Tracker" />
+            </a>
             </div>
             <div className="grid-item">
-            <a href="https://github.com/alexislendechy/Social-Network-API"></a>
+            <a href="https://github.com/alexislendechy/Social-Network-API">
+            <img className="portfolio-image" src={social} alt="Social Network API" />
+            </a>
             </div>
             <div className="grid-item">
-            <a href="https://github.com/alexislendechy/jate"></a>
+            <a href="https://github.com/alexislendechy/jate">
+            <img className="portfolio-image" src={text} alt="Text Editor" />
+            </a>
+            </div>
+            <div className="grid-item">
+            <a href="https://github.com/alexislendechy/LogoGenerator">
+            <img className="portfolio-image" src={logo} alt="Logo Generator" />
+            </a>
+            </div>
+            <div className="grid-item">
+            <a href="https://github.com/alexislendechy/READMEGenerator">
+            <img className="portfolio-image" src={read} alt="Readme Generator" />
+            </a>
             </div>
             </div>
         </section>
